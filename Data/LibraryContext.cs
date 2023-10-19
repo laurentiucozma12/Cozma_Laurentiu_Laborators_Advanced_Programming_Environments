e0d888f37b1976d60d7931ea7e7bc9c5cc087221
Lau <laurentiucozma12@gmail.com>
2023-10-19 19:51:38 +0300

    Add project files.

---
+++ Data/LibraryContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Cozma_Laurentiu_Lab2.Models;
+
+namespace Cozma_Laurentiu_Lab2.Data
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Book> Books { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>().ToTable("Customers");
+            modelBuilder.Entity<Order>().ToTable("Orders");
+            modelBuilder.Entity<Book>().ToTable("Books");
+        }
+
+    }
+}
