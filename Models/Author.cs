774faa35505eecac2428bd2cb9ba4722b68e647c
Lau <laurentiucozma12@gmail.com>
2023-10-22 12:51:31 +0300

    ex 15 sarcina 1:  clasa author adaugata, clasa books modificata, relatia la one to many

---
+++ Models/Author.cs
@@ -0,0 +1,9 @@
+﻿namespace Cozma_Laurentiu_Lab2.Models
+{
+    public class Author
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
--- Models/Book.cs
+++ Models/Book.cs
@@ -9,7 +9,8 @@ namespace Cozma_Laurentiu_Lab2.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
-        public string Author { get; set; }
+        public int AuthorID { get; set; }
+        public Author Author { get; set; }
         public decimal Price { get; set; }
     
         public ICollection<Order> Orders { get; set; }
