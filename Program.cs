a5044c192d91dfa818f8f80442c7cd1ea2c70e17
Lau <laurentiucozma12@gmail.com>
2023-10-22 12:43:06 +0300

    ex 14.

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<LibraryContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
-
+ 
 using (var scope = app.Services.CreateScope()) 
 {
     var services = scope.ServiceProvider;
