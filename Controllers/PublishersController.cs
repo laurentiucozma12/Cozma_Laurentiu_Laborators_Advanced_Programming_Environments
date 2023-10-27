ae26617221c84012d074df2eeb4cdc6d635b7355
Lau <laurentiucozma12@gmail.com>
2023-10-27 15:25:24 +0300

    lab5 complet rezolvat

--- Controllers/PublishersController.cs
+++ Controllers/PublishersController.cs
@@ -101,12 +101,39 @@ namespace Cozma_Laurentiu_Lab2.Controllers
                 return NotFound();
             }
 
-            var publisher = await _context.Publishers.FindAsync(id);
+            //var publisher = await _context.Publishers
+                //.FindAsync(id);
+            
+            var publisher = await _context.Publishers
+                    .Include(i => i.PublishedBooks).ThenInclude(i => i.Book)
+                 .AsNoTracking()
+                 .FirstOrDefaultAsync(m => m.Id == id);
+
             if (publisher == null)
             {
                 return NotFound();
             }
+
+            PopulatePublishedBookData(publisher);
             return View(publisher);
+        }
+        private void PopulatePublishedBookData(Publisher publisher)
+        {
+            var allBooks = _context.Books;
+            var publisherBooks = new HashSet<int>(publisher.PublishedBooks.Select(c => c.BookId));
+            var viewModel = new List<PublishedBookData>(); 
+
+            foreach (var book in allBooks)
+            {
+                viewModel.Add(new PublishedBookData
+                {
+                    BookId = book.Id,
+                    Title = book.Title,
+                    IsPublished = publisherBooks.Contains(book.Id)
+                });
+            }
+
+            ViewData["Books"] = viewModel;
         }
 
         // POST: Publishers/Edit/5
@@ -114,35 +141,104 @@ namespace Cozma_Laurentiu_Lab2.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,PublisherName,Address")] Publisher publisher)
+        public async Task<IActionResult> Edit(int? id, string[] selectedBooks)
         {
-            if (id != publisher.Id)
+            if (id == null)
             {
                 return NotFound();
             }
 
-            if (ModelState.IsValid)
+            var publisherToUpdate = await _context.Publishers
+                .Include(i => i.PublishedBooks)
+                .ThenInclude(i => i.Book)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (await TryUpdateModelAsync<Publisher>(publisherToUpdate, "", i => i.PublisherName, i => i.Address))
             {
+                UpdatePublishedBooks(selectedBooks, publisherToUpdate);
                 try
                 {
-                    _context.Update(publisher);
                     await _context.SaveChangesAsync();
                 }
-                catch (DbUpdateConcurrencyException)
+                catch (DbUpdateException /* ex */)
                 {
-                    if (!PublisherExists(publisher.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
+                    ModelState.AddModelError("", "Unable to save changes. " + "Try again, and if the problem persists, ");
                 }
+
                 return RedirectToAction(nameof(Index));
             }
-            return View(publisher);
+
+            UpdatePublishedBooks(selectedBooks, publisherToUpdate);
+            PopulatePublishedBookData(publisherToUpdate);
+
+            return View(publisherToUpdate);
         }
+        private void UpdatePublishedBooks(string[] selectedBooks, Publisher publisherToUpdate)
+        {
+            if (selectedBooks == null)
+            {
+                publisherToUpdate.PublishedBooks = new List<PublishedBook>();
+                return;
+            }
+            var selectedBooksHS = new HashSet<string>(selectedBooks);
+            var publishedBooks = new HashSet<int>(publisherToUpdate.PublishedBooks.Select(c => c.Book.Id));
+
+            foreach (var book in _context.Books)
+            {
+                if (selectedBooksHS.Contains(book.Id.ToString()))
+                {
+                    if (!publishedBooks.Contains(book.Id))
+                    {
+                        publisherToUpdate.PublishedBooks.Add(new PublishedBook
+                        {
+                            PublisherId =
+                            publisherToUpdate.Id,
+                            BookId = book.Id
+                        });
+                    }
+                }
+                else
+                {
+                    if (publishedBooks.Contains(book.Id))
+                    {
+                        PublishedBook bookToRemove = publisherToUpdate.PublishedBooks.FirstOrDefault(i => i.BookId == book.Id);
+                        _context.Remove(bookToRemove);
+                    }
+                }
+            }
+        }
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Edit(int id, [Bind("Id,PublisherName,Address")] Publisher publisher)
+        //{
+        //    if (id != publisher.Id)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+        //            _context.Update(publisher);
+        //            await _context.SaveChangesAsync();
+        //        }
+        //        catch (DbUpdateConcurrencyException)
+        //        {
+        //            if (!PublisherExists(publisher.Id))
+        //            {
+        //                return NotFound();
+        //            }
+        //            else
+        //            {
+        //                throw;
+        //            }
+        //        }
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(publisher);
+        //}
 
         // GET: Publishers/Delete/5
         public async Task<IActionResult> Delete(int? id)
---
+++ Models/LibraryViewModels/PublishedBookData.cs
@@ -0,0 +1,9 @@
+﻿namespace Cozma_Laurentiu_Lab2.Models.LibraryViewModels
+{
+    public class PublishedBookData
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public bool IsPublished { get; set; }
+    }
+}
